f438009297cf6b73f8e5dc37c0cf98eb01cdacf7
Rover <roverlb@live.com>
2021-10-05 01:33:56 -0500

    implementacion de clases de persistencia

--- Capa4_Persistencia/GestorSQLServer.cs
+++ Capa4_Persistencia/GestorSQLServer.cs
@@ -1,12 +1,139 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Capa4_Persistencia
 {
-    class GestorSQLServer
+    public class GestorSQLServer
     {
+        private SqlConnection conexion;
+        private SqlTransaction transaccion;
+
+        public void AbrirConexion()
+        {
+            try
+            {
+                conexion = new SqlConnection();
+                conexion.ConnectionString = "Data Source=(local);Initial Catalog=INTENSITY;Integrated Security=true";
+                conexion.Open();
+            }
+            catch (Exception err)
+            {
+                throw new Exception("Error en la conexión con la Base de Datos.", err);
+            }
+
+        }
+
+        public void CerrarConexion()
+        {
+            try
+            {
+                conexion.Close();
+            }
+            catch (Exception err)
+            {
+                throw new Exception("Error al cerrar la conexión con la Base de Datos.", err);
+            }
+
+        }
+
+        public void IniciarTransaccion()
+        {
+            try
+            {
+                AbrirConexion();
+                transaccion = conexion.BeginTransaction();
+            }
+            catch (Exception err)
+            {
+                throw new Exception("Error al iniciar la transacción con la Base de Datos.", err);
+            }
+        }
+
+        public void TerminarTransaccion()
+        {
+            try
+            {
+                transaccion.Commit();
+                conexion.Close();
+            }
+            catch (Exception err)
+            {
+                throw new Exception("Error al terminar la transacción con la Base de Datos.", err);
+            }
+        }
+
+        public void CancelarTransaccion()
+        {
+            try
+            {
+                transaccion.Rollback();
+                conexion.Close();
+            }
+            catch (Exception err)
+            {
+                throw new Exception("Error al cancelar la transacción con la Base de Datos.", err);
+            }
+        }
+
+        public SqlDataReader EjecutarConsulta(string sentenciaSQL)
+        {
+            try
+            {
+                SqlCommand comandoSQL = conexion.CreateCommand();
+                if (transaccion != null)
+                    comandoSQL.Transaction = transaccion;
+                comandoSQL.CommandText = sentenciaSQL;
+                comandoSQL.CommandType = CommandType.Text;
+                return comandoSQL.ExecuteReader();
+            }
+            catch (Exception err)
+            {
+                throw new Exception("Error al ejecutar consulta en la Base de Datos.", err);
+            }
+        }
+
+        public SqlCommand ObtenerComandoSQL(string sentenciaSQL)
+        {
+            try
+            {
+                SqlCommand comandoSQL = conexion.CreateCommand();
+                if (transaccion != null)
+                    comandoSQL.Transaction = transaccion;
+                comandoSQL.CommandText = sentenciaSQL;
+                comandoSQL.CommandType = CommandType.Text;
+                return comandoSQL;
+            }
+            catch (Exception err)
+            {
+                throw new Exception("Error al ejecutar comando en la Base de Datos.", err);
+            }
+        }
+
+        public SqlCommand ObtenerComandoDeProcedimiento(string procedimientoAlmacenado)
+        {
+            try
+            {
+                SqlCommand comandoSQL = conexion.CreateCommand();
+                if (transaccion != null)
+                    comandoSQL.Transaction = transaccion;
+                comandoSQL.CommandText = procedimientoAlmacenado;
+                comandoSQL.CommandType = CommandType.StoredProcedure;
+                return comandoSQL;
+            }
+            catch (Exception err)
+            {
+                throw new Exception("Error al ejecutar comando en la Base de Datos.", err);
+            }
+        }
+
+
+
+
+
     }
 }
