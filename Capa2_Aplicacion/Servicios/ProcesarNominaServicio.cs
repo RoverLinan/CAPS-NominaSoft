49777e91086417207281371a7ef53a0cda1a6261
Rover <roverlb@live.com>
2021-10-05 02:23:55 -0500

    Actualizado

--- Capa2_Aplicacion/Servicios/ProcesarNominaServicio.cs
+++ Capa2_Aplicacion/Servicios/ProcesarNominaServicio.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Capa3_Dominio.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,37 @@ using System.Threading.Tasks;
 
 namespace Capa2_Aplicacion.Servicios
 {
-    class ProcesarNominaServicio
+    public class ProcesarNominaServicio
     {
+
+
+        public ProcesarNominaServicio()
+        {
+
+        }
+
+
+        public bool GenerarNomina(List<Contrato> listaContratos, PeriodoDeNomina periodo, Nomina nomina )
+        {
+            foreach (Contrato item in listaContratos)
+            {
+                if (item.Fechafin.CompareTo(periodo.Fechainicio) == 1 && item.Cancelado == false)
+                {
+                    return false;
+                }
+            }
+
+
+            return true;
+
+        }
+
+
+
+
+
+
+
+
     }
 }
